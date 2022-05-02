a1bc026ee386dd4abf7371cea8751b991b7f4238
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-05-01 23:51:02 -0700

    using java, testing github actions

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import query.Client;
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
---
+++ src/main/java/query/Client.java
@@ -0,0 +1,16 @@
+package query;
+
+import java.net.http.*;
+
+
+public class Client {
+    private HttpClient CLIENT;
+
+    public Client() {
+        CLIENT = null;
+    }
+
+//    public HttpResponse<String> request(String url, String query) {
+//
+//    }
+}
